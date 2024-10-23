4f110d6bdd19e77a6cef1076c5b5ac277036e05a
bouda890 <bouda8900@gmail.com>
2024-10-23 10:44:40 +0200

    added data class

--- analysis.py
+++ analysis.py
@@ -1,1 +1,41 @@
 #extracts information from dataset
+
+import numpy as np
+
+class Data():
+    def __init__(self, data_path):
+        self.data = self.load_data(data_path)
+
+
+    def load_data(self, data_path):
+        """formats data to desired format"""
+        data = ""
+        return data
+    
+    def get_train_matrix(self) -> np.array:
+        "gets train matrix, match outcome is in the last column"
+
+        features = [self.get_home_avg_points(), self.get_away_avg_points(), self.get_match_outcomes]
+
+        matrix = [func() for func in features]
+
+        return np.stack(matrix).T
+    
+    
+
+
+
+
+
+
+    
+    def get_home_avg_points() -> np.array:
+        pass
+
+    def get_away_avg_points() -> np.array:
+        pass
+
+    def get_match_outcomes() -> np.array:
+        pass
+
+        
