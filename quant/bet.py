0be425527bbd71f7cfd6ba513ef1a3846afd9856
bouda890 <bouda8900@gmail.com>
2024-11-01 13:09:59 +0100

    moddified betting to work with pandas

--- quant/bet.py
+++ quant/bet.py
@@ -2,27 +2,28 @@
 import math
 
 import numpy as np
-import scipy.optimize
 import pandas as pd
+import scipy.optimize
 
 
 class Player:
-    def __init__(self, budget):
-        self.budget = budget
-        self.R = 0
+    """Handles betting strateggy."""
 
-    def get_expected_profit(self, prob, ratio, prop_of_budget) -> float:
+    def get_expected_profit(self, prob: float, ratio: float, prop_of_budget: float) -> float:
+        """Get expected profit for given parametrs."""
         return (prob * ratio - 1) * prop_of_budget
 
-    def get_variance_of_profit(self, prob, ratio, prop_of_budget) -> float:
+    def get_variance_of_profit(self, prob: float, ratio: float, prop_of_budget: float) -> float:
+        """Get varience of profit for given parameters."""
         return (1 - prob) * prob * (prop_of_budget**2) * (ratio**2)
 
-    def sharpe_ratio(self, total_profit, total_var) -> float:
+    def sharpe_ratio(self, total_profit: float, total_var: float) -> float:
+        """Return total sharp ratio."""
         if total_var == 0:
             return np.inf
-        return (total_profit - self.R) / math.sqrt(total_var)
+        return total_profit / math.sqrt(total_var)
 
-    def min_function(self, props, probs, ratios) -> float:
+    def min_function(self, props: np.ndarray, probs: np.ndarray, ratios: np.ndarray) -> float:
         """We are trying to minimaze this function for sharp ratio."""
         total_profit = 0
         total_var = 0
@@ -37,31 +38,31 @@ class Player:
                 total_var += self.get_variance_of_profit(prob, ratio, prop_of_budget)
         return -self.sharpe_ratio(total_profit, total_var)
 
-    def get_bet_proportions(self, probs, ratios) -> np.ndarray:
+    def get_bet_proportions(self, probs: np.ndarray, active_matches: pd.DataFrame, summary: pd.DataFrame) -> np.ndarray:
         """Return proportions of the budget to bet on speific probs, (in the same format as probs)."""
         num_bets = probs.shape[0] * probs.shape[1]
         initial_props = np.full(num_bets, 1 / num_bets, dtype=float)
         # Constraint: sum of all props <= 1 (global budget constraint for entire 2D array), idk man it does something
         cons = [{"type": "ineq", "fun": lambda props: 1.0 - sum(props)}]
-        bounds = [(0, 1) for _ in range(num_bets)]
+        min_proportion = summary.iloc[0]["Min_bet"]/summary.iloc[0]["Bankroll"]
+        max_proportion = summary.iloc[0]["Max_bet"]/summary.iloc[0]["Bankroll"]
+        bounds = [(min_proportion, max_proportion) for _ in range(num_bets)]
         result = scipy.optimize.minimize(
             self.min_function,
             initial_props,
-            args=(probs, ratios),
+            args=(probs, active_matches[["oddsH", "oddsA"]].to_numpy()),
             method="SLSQP",
             bounds=bounds,
             constraints=cons,
         )
         return result.x.reshape(probs.shape)
 
-    def get_betting_strategy(self, probabbilities: np.ndarray, active_matches: pd.DataFrame) -> list:
+    def get_betting_strategy(self, probabbilities: np.ndarray, active_matches: pd.DataFrame, summary: pd.DataFrame) -> list:
         """Return absolute cash numbers and on what to bet in 2d list."""
-        proportions = self.get_bet_proportions(probabbilities, ratios)
+        proportions = self.get_bet_proportions(probabbilities, active_matches, summary)
         bets = [[0] * 2 for _ in range(len(proportions))]
         for i in range(len(proportions)):
             for j in range(2):
-                bets[i][j] = proportions[i][j] * self.budget
+                bets[i][j] = proportions[i][j] * summary.iloc[0]["Bankroll"]
         return bets
 
-    def update_budget(self, new):
-        self.budget = new
