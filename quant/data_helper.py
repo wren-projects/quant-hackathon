807d8746bb5c0a074e3422bbbe2cd220cbcc6d8c
Bonifac <kolenomi@seznam.cz>
2024-11-05 23:41:27 +0100

    add point avr ability

--- quant/data_helper.py
+++ quant/data_helper.py
@@ -2,6 +2,27 @@ import numpy as np
 import pandas as pd
 
 
+class CustomQueue:
+    """Serve as custom version of queue."""
+
+    def __init__(self, n: int) -> None:
+        """Initialize queue."""
+        self.size: int = n
+        self.values: np.array = np.zeros((n, 1))
+        self.__curent_oldest: int = 0
+
+    def put(self, value: float) -> None:
+        """Put new value in queue."""
+        self.values[self.__curent_oldest % self.size] = value
+        self.__curent_oldest += 1
+
+    def get_q_avr(self) -> np.array:
+        """Return avrage array of each feature."""
+        if self.__curent_oldest == 0:
+            return 0.0
+        return np.sum(self.values) / min(self.size, self.__curent_oldest)
+
+
 class TeamData:
     """Hold data of one team."""
 
@@ -9,74 +30,30 @@ class TeamData:
         """Init datastucture."""
         self.id = id
         self.date_last_mach: pd.Timestamp = pd.to_datetime("1975-11-06")
+        self.home_points_last_n: CustomQueue = CustomQueue(10)
+        self.away_points_last_n: CustomQueue = CustomQueue(10)
 
     def get_days_scince_last_mach(self, today: pd.Timestamp) -> int:
-        """Return number of days scince last mach and update last mach to today."""
+        """Return number of days scince last mach."""
         delta = today - self.date_last_mach
-        self.date_last_mach = today
         return delta.days
 
+    def update_data(self, data_line: pd.DataFrame) -> None:
+        """Update team data based on dato from one mach (one line of data)."""
+        self.date_last_mach = data_line["Date"]
+        # TODO update last n based on home_away
 
+    def get_data_vector(self, home_away: int) -> np.array:
+        """Return complete data vector for given team."""
+        if home_away == 0:
+            output_points = self.home_points_last_n.get_q_avr()
+        else:
+            output_points = self.away_points_last_n.get_q_avr()
+        return np.array([self.get_days_scince_last_mach, output_points])
+
+
+"""
 data = TeamData(1)
 time = pd.Timestamp("1975-11-20")
 print(data.get_days_scince_last_mach(time))
-
-
-teams_last_ten: dict = {}
-
-
-class CustomQueue:
-    """Serve as custom version of queue."""
-
-    def __init__(self, n: int, feature_count: int) -> None:
-        """Initialize queue."""
-        self.size: int = n
-        self.feature_count: int = feature_count
-        self.values: np.matrix = np.zeros((n, feature_count))
-        self.__curent_oldest: int = 0
-
-    def put(self, value: np.array) -> None:
-        """Put new value in queue."""
-        self.values[self.__curent_oldest % self.size, :] = value
-        self.__curent_oldest += 1
-
-    def get_q_avr(self) -> np.array:
-        """Return avrage array of each feature."""
-        if self.__curent_oldest == 0:
-            return np.zeros((1, self.feature_count))
-        return self.values.mean(0)
-
-
-class TeamsLastN:
-    """Contain all features needed to calculate avreages of last n matches."""
-
-    def __init__(self, n: int) -> None:
-        """Initialize."""
-        self.teams_last_n_values: dict[int, CustomQueue] = {}
-        self.n: int = n
-
-    def add_value(self, team_id: int, value: np.array) -> None:
-        """Add featuer values to dataset for given team."""
-        if team_id not in self.teams_last_n_values:
-            self.teams_last_n_values[team_id] = CustomQueue(self.n, len(value))
-        self.teams_last_n_values[team_id].put(value)
-
-    def get_avr(self, team_id: int) -> np.array:
-        """"""
-        return self.teams_last_n_values[team_id].get_q_avr()
-
-
-"""
-team = TeamsLastN(10, 2)
-for i in range(100):
-    team.add_value(0, [i, 3 * i])
-    print(team.get_avr(0))
-"""
-"""
-
-# for i in range(100):
-q = Custom_Queue(10)
-for i in range(100):
-    q.put(i)
-    print(q.get_avr())
 """
