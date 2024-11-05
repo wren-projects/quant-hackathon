9091d91295aedefab2a45c2b42b4811aa46a23b5
Bonifac <kolenomi@seznam.cz>
2024-11-05 22:46:09 +0100

    add time feture

--- quant/data_helper.py
+++ quant/data_helper.py
@@ -1,4 +1,26 @@
 import numpy as np
+import pandas as pd
+
+
+class TeamData:
+    """Hold data of one team."""
+
+    def __init__(self, id: int):
+        """Init datastucture."""
+        self.id = id
+        self.date_last_mach: pd.Timestamp = pd.to_datetime("1975-11-06")
+
+    def get_days_scince_last_mach(self, today: pd.Timestamp) -> int:
+        """Return number of days scince last mach and update last mach to today."""
+        delta = today - self.date_last_mach
+        self.date_last_mach = today
+        return delta.days
+
+
+data = TeamData(1)
+time = pd.Timestamp("1975-11-20")
+print(data.get_days_scince_last_mach(time))
+
 
 teams_last_ten: dict = {}
 
@@ -26,17 +48,21 @@ class CustomQueue:
 
 
 class TeamsLastN:
-    def __init__(self, n: int, feature_count: int) -> None:
+    """Contain all features needed to calculate avreages of last n matches."""
+
+    def __init__(self, n: int) -> None:
+        """Initialize."""
         self.teams_last_n_values: dict[int, CustomQueue] = {}
         self.n: int = n
-        self.feature_count = feature_count
 
     def add_value(self, team_id: int, value: np.array) -> None:
+        """Add featuer values to dataset for given team."""
         if team_id not in self.teams_last_n_values:
-            self.teams_last_n_values[team_id] = CustomQueue(self.n, self.feature_count)
+            self.teams_last_n_values[team_id] = CustomQueue(self.n, len(value))
         self.teams_last_n_values[team_id].put(value)
 
     def get_avr(self, team_id: int) -> np.array:
+        """"""
         return self.teams_last_n_values[team_id].get_q_avr()
 
 
