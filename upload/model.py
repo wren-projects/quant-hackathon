6dd23bf181731e00f0ff92516e547c89febf202e
Maneren <maneren731@gmail.com>
2024-11-09 15:15:21 +0100

    move data to submission file

--- upload/model.py
+++ upload/model.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import math
 from collections import namedtuple
+from enum import IntEnum
 from itertools import product
 from typing import TYPE_CHECKING, Protocol, TypeAlias
 
@@ -369,10 +370,6 @@ class Model:
         self.ai.train(train_matrix)
 
 
-# Merging predict.py
-# handles predicting results
-
-
 class Ai:
     """Class for training and predicting."""
 
@@ -404,3 +401,128 @@ class Ai:
     def save_model(self, path: os.PathLike) -> None:
         """Save ML model."""
         self.model.save_model(path)
+
+
+class Team(IntEnum):
+    """Enum discerning teams playing home or away."""
+
+    Home = 0
+    Away = 1
+
+
+class CustomQueue:
+    """Serve as custom version of queue."""
+
+    def __init__(self, n: int) -> None:
+        """Initialize queue."""
+        self.size: int = n
+        self.values: np.array = np.zeros((n, 1))
+        self.__curent_oldest: int = 0
+
+    def put(self, value: float) -> None:
+        """Put new value in queue."""
+        self.values[self.__curent_oldest % self.size] = value
+        self.__curent_oldest += 1
+
+    def get_q_avr(self) -> float:
+        """Return average array of each feature."""
+        if self.__curent_oldest == 0:
+            return 0.0
+
+        return (
+            np.sum(self.values) / min(self.size, self.__curent_oldest)
+            if self.__curent_oldest
+            else 0.0
+        )
+
+
+class TeamData:
+    """Hold data of one team, both as home and away."""
+
+    N_SHORT = 8
+    N_LONG = 20
+
+    COLUMNS = 2
+
+    def __init__(self, team_id: TeamID) -> None:
+        """Init datastucture."""
+        self.id: TeamID = team_id
+        self.date_last_mach: pd.Timestamp = pd.to_datetime("1975-11-06")
+        self.home_points_last_n: CustomQueue = CustomQueue(TeamData.N_SHORT)
+        self.away_points_last_n: CustomQueue = CustomQueue(TeamData.N_SHORT)
+
+    def _get_days_since_last_mach(self, today: pd.Timestamp) -> int:
+        """Return number of days scince last mach."""
+        return (today - self.date_last_mach).days
+
+    def update(self, match: Match, played_as: Team) -> None:
+        """Update team data based on data from one mach."""
+        self.date_last_mach = pd.to_datetime(match.Date)
+
+        if played_as == Team.Home:
+            self.home_points_last_n.put(match.HSC)
+        else:
+            self.away_points_last_n.put(match.ASC)
+
+    def get_data_vector(self, played_as: Team, date: pd.Timestamp) -> np.ndarray:
+        """
+        Return complete data vector for given team.
+
+        Return vector:[
+        days scine last mach,
+        avr points in lasnt n matches as H/A
+        ]
+        """
+        points = (
+            self.home_points_last_n
+            if played_as == Team.Home
+            else self.away_points_last_n
+        )
+
+        output_points = points.get_q_avr()
+
+        last_date = self._get_days_since_last_mach(date)
+
+        return np.array([last_date, output_points])
+
+
+class Data:
+    """Class for working with data."""
+
+    def __init__(self, data: pd.DataFrame) -> None:
+        """Create Data from csv file."""
+        self.data = data
+        self.teams_data: dict[TeamID, TeamData] = {}
+
+    def _get_match_array(self, match: Match) -> np.ndarray:
+        """
+        Return array for specific match and update team data.
+
+        Based on matches that happend so far.
+        Used for making training matrix.
+        """
+        date: pd.Timestamp = pd.to_datetime(match.Date)
+
+        home_team = self.teams_data.setdefault(match.HID, TeamData(match.HID))
+        away_team = self.teams_data.setdefault(match.AID, TeamData(match.AID))
+
+        output: np.ndarray = np.concatenate(
+            (
+                home_team.get_data_vector(Team.Home, date),
+                away_team.get_data_vector(Team.Away, date),
+            )
+        )
+
+        home_team.update(match, Team.Home)
+        away_team.update(match, Team.Away)
+
+        return output
+
+    def get_train_matrix(self) -> np.ndarray:
+        """Create train matrix from the current data."""
+        train_matrix: np.ndarray = np.empty((len(self.data), 2 * TeamData.COLUMNS))
+
+        for match in (Match(*row) for row in self.data.itertuples(index=True)):
+            train_matrix[match.Index] = self._get_match_array(match)
+
+        return train_matrix
