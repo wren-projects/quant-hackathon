f4d832e874d5fd50d0a21a7a906e70d3b993654d
gavlna <kopciljakub@gmail.com>
2024-11-17 22:36:16 +0100

    Added OpenRating
    modely:
    TM má otřesnou distribuci (velmi center heavy)
    PlackettLuce a BradleyTerryFull mají krásnou distribuci, ale pouze pokud jsou sezóny nezávislé nebo je mezi nimi velký boost ve variaci

--- upload/analyze.py
+++ upload/analyze.py
@@ -1,34 +1,97 @@
 import sys
 import math
-
+import matplotlib.pyplot as plt
+from openskill.models import PlackettLuce, BradleyTerryFull, BradleyTerryPart
 import pandas as pd
 import numpy as np
-sys.path.append(".")
 
-from environment import Environment
-from model import Model
+sys.path.append(".")
 
 
 data = []
 
-bets = [[0,0],[0,0]]
-    
+bets = [[0, 0], [0, 0]]
+
 K = 40
+
+
+openSkillRating = {}
+
+
+def openSkillAnalyze(season: pd.DataFrame):
+    # tracks accuracy
+    # on i-th position is a tuple:
+    # 0) how many times did home win when his predicted winrate was i%
+    # 1) how many times was home's predicted winrate i%
+    global openSkillRating
+
+    predictionDif = np.array([np.zeros(2) for i in range(100)])
+    predictionCount = np.array([np.zeros(1) for i in range(100)])
+    model = BradleyTerryFull()
+
+    x = season.groupby("AID").groups.keys()
+    rating = {i: model.rating(name=i) for i in x}
+    rating.update(openSkillRating)
+
+    for i in rating:
+        rating[i].sigma = model.sigma
+    # count number of matches in this season
+    i = 0
+    for _, match in season.iterrows():
+        i += 1
+        hId = match["HID"]
+        aId = match["AID"]
+        hScore = match["H"]
+        aScore = match["A"]
+
+        eloH = [rating[hId]]
+        # eloA = [awayElo[aId]]
+        eloA = [rating[aId]]
+
+        # calculates the odds of home to win
+        expected = model.predict_win([eloH, eloA])[0]
+        # adjusts elo
+        if hScore:
+            [[newHome], [newAway]] = model.rate([eloH, eloA])
+        else:
+            [[newAway], [newHome]] = model.rate([eloA, eloH])
+
+        rating[hId] = newHome
+        rating[aId] = newAway
+
+        # if elo had enough time to stabilize, adjust expected dif
+        realWinner = hScore > aScore
+        predictionDif[min(99, int(expected * 100))][0] += realWinner
+        predictionDif[min(99, int(expected * 100))][1] += 1
+        predictionCount[min(99, int(expected * 100))] += 1
+
+        # bet home
+        if expected > 0.7:
+            bets[0][aScore] += 1
+        if expected < 0.3:
+            bets[1][aScore] += 1
+
+    openSkillRating.update(rating)
+    return predictionCount
+    return predictionDif
+
+
 def eloAnalyze(season: pd.DataFrame):
     # tracks accuracy
     # on i-th position is a tuple:
     # 0) how many times did home win when his predicted winrate was i%
     # 1) how many times was home's predicted winrate i%
 
+    predictionCount = np.array([np.zeros(1) for i in range(100)])
     eloDif = np.array([np.zeros(2) for i in range(100)])
 
     # calculates odds
     def helper(eloH, eloA):
-        d = eloA-eloH
-        #d = max(min(d,800), -800)
-        A = 160**((d)/400)
-        return 1/(1+A)
-    
+        d = eloA - eloH
+        # d = max(min(d,800), -800)
+        A = 160 ** ((d) / 400)
+        return 1 / (1 + A)
+
     # setups everyone's elo to 1000
     x = season.groupby("AID").groups.keys()
     homeElo = {i: 1000 for i in x}
@@ -47,25 +110,25 @@ def eloAnalyze(season: pd.DataFrame):
         eloA = awayElo[aId]
 
         # calculates the odds of home to win
-        expected = helper(eloH,eloA)
+        expected = helper(eloH, eloA)
 
         # adjusts elo
-        homeElo[hId] += (hScore-expected) * K
-        awayElo[aId] += (aScore-(1-expected)) * K
+        homeElo[hId] += (hScore - expected) * K
+        awayElo[aId] += (aScore - (1 - expected)) * K
 
-        
         # if elo had enough time to stabilize, adjust expected dif
         if i > 400:
             realWinner = hScore > aScore
-            eloDif[int(expected*100)][0] += realWinner
-            eloDif[int(expected*100)][1] += 1
+            eloDif[int(expected * 100)][0] += realWinner
+            eloDif[int(expected * 100)][1] += 1
+            predictionCount[int(expected * 100)] += 1
 
-    return eloDif
+    return predictionCount
 
 
 def glicoAnalyze(season: pd.DataFrame):
     C = 35
-    q = math.log(10)/1000
+    q = math.log(10) / 1000
     # tracks accuracy
     # on i-th position is a tuple:
     # 0) how many times did home win when his predicted winrate was i%
@@ -74,23 +137,32 @@ def glicoAnalyze(season: pd.DataFrame):
     eloDif = np.array([np.zeros(2) for i in range(100)])
 
     def get_g(RD):
-        return 1/math.sqrt(1 + 3 * q**2 * RD**2/math.pi**2)
-    
+        return 1 / math.sqrt(1 + 3 * q**2 * RD**2 / math.pi**2)
+
     # calculates odds
     def helper(ratH, ratA):
-        return 1/(1 + 10**(get_g(ratA[1])*(ratH[0]-ratA[1])/(-400)))
-    
-    def adjustRating(ratH: tuple[int, int], ratA: tuple[int, int], daysSinceLastGame: int, result: bool):
-        RD = min(math.sqrt(ratH[1]**2+C**2*daysSinceLastGame),350)
-        d_squared = 1/(q**2 * get_g(ratA[1])**2 * helper(ratH, ratA) * (1 - helper(ratH, ratA)))
-        newR = ratH[0] + q/(1/RD**2 + 1/d_squared) * get_g(ratA[1])*(result - helper(ratH, ratA))
-        newRD = 1/math.sqrt(1/RD**2 + 1/d_squared)
+        return 1 / (1 + 10 ** (get_g(ratA[1]) * (ratH[0] - ratA[1]) / (-400)))
+
+    def adjustRating(
+        ratH: tuple[int, int],
+        ratA: tuple[int, int],
+        daysSinceLastGame: int,
+        result: bool,
+    ):
+        RD = min(math.sqrt(ratH[1] ** 2 + C**2 * daysSinceLastGame), 350)
+        d_squared = 1 / (
+            q**2 * get_g(ratA[1]) ** 2 * helper(ratH, ratA) * (1 - helper(ratH, ratA))
+        )
+        newR = ratH[0] + q / (1 / RD**2 + 1 / d_squared) * get_g(ratA[1]) * (
+            result - helper(ratH, ratA)
+        )
+        newRD = 1 / math.sqrt(1 / RD**2 + 1 / d_squared)
         return [newR, newRD]
 
     # setups everyone's elo to 1000
     x = season.groupby("AID").groups.keys()
-    homeGlico = {i: [1500,350] for i in x}
-    awayGlico = {i: [1500,350] for i in x}
+    homeGlico = {i: [1500, 350] for i in x}
+    awayGlico = {i: [1500, 350] for i in x}
     lastGameHome = {i: pd.Timestamp(0) for i in x}
     lastGameAway = {i: pd.Timestamp(0) for i in x}
     # count number of matches in this season
@@ -106,20 +178,23 @@ def glicoAnalyze(season: pd.DataFrame):
         eloA = awayGlico[aId]
 
         # calculates the odds of home to win
-        expected = helper(eloH,eloA)
+        expected = helper(eloH, eloA)
 
         # adjusts elo
-        homeGlico[hId] = adjustRating(eloH, eloA, (date-lastGameHome[hId]).days, hScore > aScore)
-        awayGlico[aId] = adjustRating(eloA, eloH, (date-lastGameAway[aId]).days, hScore < aScore)
+        homeGlico[hId] = adjustRating(
+            eloH, eloA, (date - lastGameHome[hId]).days, hScore > aScore
+        )
+        awayGlico[aId] = adjustRating(
+            eloA, eloH, (date - lastGameAway[aId]).days, hScore < aScore
+        )
         lastGameHome[hId] = date
         lastGameAway[hId] = date
 
-        
         # if elo had enough time to stabilize, adjust expected dif
         if i > 200:
             realWinner = hScore > aScore
-            eloDif[int(expected*100)][0] += realWinner
-            eloDif[int(expected*100)][1] += 1
+            eloDif[int(expected * 100)][0] += realWinner
+            eloDif[int(expected * 100)][1] += 1
 
     return eloDif
 
@@ -127,15 +202,56 @@ def glicoAnalyze(season: pd.DataFrame):
 ##################
 # IGNORE FOR NOW #
 ##################
+def assignOpen(season: pd.DataFrame):
+    global openSkillRating
+
+    predictionDif = np.array([np.zeros(2) for i in range(100)])
+    predictionCount = np.array([np.zeros(1) for i in range(100)])
+    model = ThurstoneMostellerFull()
+
+    x = season.groupby("AID").groups.keys()
+    rating = {i: model.rating(name=i) for i in x}
+    rating.update(openSkillRating)
+
+    for i in rating:
+        rating[i].sigma += model.sigma / 18
+
+    i = 0
+    x = []
+    for _, match in season.iterrows():
+        i += 1
+        hId = match["HID"]
+        aId = match["AID"]
+        hScore = match["H"]
+        aScore = match["A"]
+
+        eloH = [rating[hId]]
+        # eloA = [awayElo[aId]]
+        eloA = [rating[aId]]
+
+        # calculates the odds of home to win
+        expected = model.predict_win([eloH, eloA])[0]
+        # adjusts elo
+        if hScore:
+            [[newHome], [newAway]] = model.rate([eloH, eloA])
+        else:
+            [[newAway], [newHome]] = model.rate([eloA, eloH])
+
+        rating[hId] = newHome
+        rating[aId] = newAway
+        x.append(expected)
+
+    season["RATING"] = x
+    return season
+
+
 def assignElo(season: pd.DataFrame):
-    guess = [[0,0],[0,0]]
-    bets = [0,0]
     def helper(eloH, eloA):
-        d = eloA-eloH
-        #d = max(min(d,800), -800)
-        A = 160**((d)/400)
-        return 1/(1+A)
-    
+        d = eloA - eloH
+        # d = max(min(d,800), -800)
+        A = 160 ** ((d) / 400)
+        return 1 / (1 + A)
+
     x = season.groupby("AID").groups.keys()
     homeElo = {i: 1000 for i in x}
     awayElo = {i: 1000 for i in x}
@@ -145,27 +261,23 @@ def assignElo(season: pd.DataFrame):
         c += 1
         hId = match["HID"]
         aId = match["AID"]
-        betsH = match["OddsH"]
-        betsA = match["OddsA"]
         hScore = match["H"]
         aScore = match["A"]
 
         eloH = homeElo[hId]
         eloA = awayElo[aId]
 
-        expected = helper(eloH,eloA)
+        expected = helper(eloH, eloA)
 
-        homeElo[hId] += (hScore-expected) * K
-        awayElo[aId] += (aScore-(1-expected)) * K
+        homeElo[hId] += (hScore - expected) * K
+        awayElo[aId] += (aScore - (1 - expected)) * K
         if i > 200:
             x.append(expected)
         else:
             x.append(None)
-        
 
     season["Elo"] = x
     return season
-
 
 
 # load everything
@@ -178,66 +290,80 @@ players = pd.read_csv("./data/players.csv", index_col=0)
 players["Date"] = pd.to_datetime(players["Date"])
 
 
-
 withElo = pd.read_csv("./data/withelo.csv", index_col=0)
 withElo["Date"] = pd.to_datetime(games["Date"])
 withElo["Open"] = pd.to_datetime(games["Open"])
+withElo["RATING"] = withElo["Elo"]
+withRating = pd.read_csv("./data/withRating.csv", index_col=0)
+withRating["Date"] = pd.to_datetime(games["Date"])
+withRating["Open"] = pd.to_datetime(games["Open"])
 
 
-def runSeason(function):
+def runSeason(function: callable) -> any:
     x = None
     for i, season in games.groupby("Season"):
-        #print("SEASON", i)
+        # print("SEASON", i)
         if x is None:
             x = function(season)
-        elif type(x) == pd.DataFrame:
-            x = pd.concat([x,function(season)])
+        elif type(x) is pd.DataFrame:
+            x = pd.concat([x, function(season)])
         else:
             x += function(season)
     return x
 
 
-
-
 # RELEVANT MAIN
-def analyzeElo():
+def analyzeElo() -> None:
     eloDif = runSeason(eloAnalyze)
-    a = eloDif[:,0]
-    b = eloDif[:,1]
-    winrate = a/b
+    plt.plot(range(100), eloDif)
+    return
+    a = eloDif[:, 0]
+    b = eloDif[:, 1]
+    winrate = a / b
 
     # filter out all NaNs
     mask = np.logical_not(np.isnan(winrate))
-    actual = winrate[mask]*100
+    actual = winrate[mask] * 100
     expected = np.arange(100)[mask]
 
-    print(np.corrcoef(actual, expected))    
-    dif = actual-expected
+    print(np.corrcoef(actual, expected))
+    dif = actual - expected
     print(np.var(dif))
-    print(sum(dif)/len(dif))
+    print(sum(dif) / len(dif))
     print(dict(zip(expected, actual)))
 
 
-def analyzeWithElo():
-    matrix = [[0,0], [0,0], [0,0]]
+def analyze_with_elo() -> None:
+    matrix = [[0, 0], [0, 0], [0, 0]]
     predicted = pd.DataFrame()
-    predicted["P"] = withElo["Elo"].map(lambda a: 0 if a<0.3 else (1 if a <0.7 else 2))
+    predicted["P"] = withElo["Elo"].map(
+        lambda a: 0 if a < 0.3 else (1 if a < 0.7 else 2)
+    )
     predicted["R"] = withElo["H"]
-    for i,p in predicted.iterrows():
+    for i, p in predicted.iterrows():
         matrix[p["P"]][p["R"]] += 1
 
-def tryBets():
+
+def try_bets() -> None:
     # Home
     home = pd.DataFrame()
-
-    #home["B"] = withElo.apply(lambda line: 1 if 1/line["OddsH"] < 0.25 and line["Elo"] * line["OddsH"] > 2 else 0,axis=1)
-    for i in range(100, 300, 1):
-        i = i/100
-        home["B"] = withElo.apply(lambda line: 1 if line["Elo"] * line["OddsH"] > i else 0,axis=1)
+    profit = []
+    success = []
+    for i in range(100, 300, 2):
+        print(i)
+        i = i / 100  # noqa: PLW2901
+        home["B"] = withRating.apply(
+            lambda line, i=i: 1 if line["RATING"] * line["OddsH"] > i else 0, axis=1
+        )
         home["Win"] = home["B"] * withElo["H"]
         home["Profit"] = home["B"] * withElo["H"] * withElo["OddsH"]
-        print(i,f'won {home["Win"].sum()}/{home["B"].sum()} [{home["Profit"].sum()}] = {home["Profit"].sum() - home["B"].sum()}')
-
+        profit.append(home["Profit"].sum() - home["B"].sum())
+        success.append(home["Win"].sum() / home["B"].sum())
+    print(max(profit))
+    plt.plot(range(100, 300, 2), profit)
+    plt.plot(range(100, 300, 2), success)
+    plt.legend(["Profit", "success"])
+    plt.show()
     """
     #Away
     away = pd.DataFrame()
@@ -251,39 +377,60 @@ def tryBets():
     print("won", away["Profit"].sum())
     """
 
-def analyzeBets():
+
+def analyzeBets() -> None:
     x = pd.DataFrame()
-    x["Elo"] = withElo["Elo"]
-    x["H"] = withElo["H"]
-    x["N"] = withElo["N"]
-    x["POFF"] = withElo["POFF"]
-    x["OddsH"] = withElo["OddsH"]
-    x["OddsA"] = withElo["OddsA"]
-    x["RequiredH"] = 1/x["OddsH"]
-    x["RequiredA"] = 1/x["OddsA"]
-    print(x.nsmallest(50,"RequiredH"))
-    print(x.nsmallest(50,"RequiredA"))
-    
+    x["Elo"] = withRating["RATING"]
+    x["H"] = withRating["H"]
+    x["N"] = withRating["N"]
+    x["POFF"] = withRating["POFF"]
+    x["OddsH"] = withRating["OddsH"]
+    x["OddsA"] = withRating["OddsA"]
+    x["RequiredH"] = 1 / x["OddsH"]
+    x["RequiredA"] = 1 / x["OddsA"]
+    print(x.nsmallest(50, "RequiredH"))
+    print(x.nsmallest(50, "RequiredA"))
 
 
-
-
-def anaylzeGlico():
+def anaylzeGlico() -> None:
     eloDif = runSeason(glicoAnalyze)
-    a = eloDif[:,0]
-    b = eloDif[:,1]
-    winrate = a/b
+    a = eloDif[:, 0]
+    b = eloDif[:, 1]
+    winrate = a / b
 
     # filter out all NaNs
     mask = np.logical_not(np.isnan(winrate))
-    actual = winrate[mask]*100
+    actual = winrate[mask] * 100
     expected = np.arange(100)[mask]
 
-    print(np.corrcoef(actual, expected))    
-    dif = actual-expected
+    print(np.corrcoef(actual, expected))
+    dif = actual - expected
     print(np.var(dif))
-    print(sum(dif)/len(dif))
+    print(sum(dif) / len(dif))
     print(dict(zip(expected, actual)))
 
 
-analyzeElo()
+def analyzeOpenSKill() -> None:
+    eloDif = runSeason(openSkillAnalyze)
+    plt.plot(range(100), eloDif)
+    plt.show()
+    a = eloDif[:, 0]
+    b = eloDif[:, 1]
+    winrate = a / b
+
+    # filter out all NaNs
+    mask = np.logical_not(np.isnan(winrate))
+    actual = winrate[mask] * 100
+    expected = np.arange(100)[mask]
+    print(np.corrcoef(actual, expected))
+    dif = actual - expected
+    print(np.var(dif))
+    plt.plot(range(100), actual)
+    print(sum(dif) / len(dif))
+    # print(dict(zip(expected, actual)))
+
+
+analyzeOpenSKill()
+# plt.plot(range(100), range(100))
+# print(bets)
+# try_bets()
